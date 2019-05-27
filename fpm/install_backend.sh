ba6ee4294831177271e3da3d886c5e7be18c2ad0
matrixlord <th.nokas@gmail.com>
2019-05-28 00:53:24 +0300

    Initial commit

---
+++ fpm/install_backend.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Check DBs are initialized.
+until nc -z -v -w30 db 3306
+do
+	echo "Waiting for database connection..."
+  	# wait for 5 seconds before check again
+  	sleep 5
+done
+
+HOME_ROOT="/home/serveruser/www/backend"
+
+# Fix permissions.
+cd $HOME_ROOT
+chown -R serveruser:serveruser .
+chmod 755 $HOME_ROOT/web/sites/default
+
+composer install
+drush site:install -y --site-name=Backend minimal --account-name=admin --account-pass=123456 --config-dir='/home/serveruser/vhosts/wb/configuration/sync' --db-url='mysql://'"$MYSQL_USER"':'"$MYSQL_PASSWORD"'@db/'"$MYSQL_DATABASE"''
+
+drush site:install -y --site-name=Backend minimal --account-name=admin --account-pass=123456 --config-dir='/home/serveruser/www/backend/configuration/sync' --db-url='mysql://'"$MYSQL_USER"':'"$MYSQL_PASSWORD"'@db/'"$MYSQL_DATABASE"''
+
+# Change settings path.
+cd $HOME_ROOT/web/sites/default
+chmod 755 .
+chmod 644 settings.php
+sed -i -e"s/^\$config_directories.*sync.*=.*/\$config_directories['sync'] = '\/home\/serveruser\/vhosts\/wb\/configuration\/sync';/g" settings.php
+drush state-set system.cron_key $DRUPAL_CRON_KEY
+chmod 555 .
+chmod 444 settings.php
+
+# Import settings.
+drush cr
+drush cim -y
+
+# Delete old folder.
+rm -rf $HOME_ROOT/config
